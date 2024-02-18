d69112617c27951e727ef859c851c913f08c6a70
nosti <julie.hubert@epfedu.fr>
2024-02-18 22:37:46 +0100

    Sceance1.exercice3.2

--- src/main/java/com/epf/rentmanager/service/ClientService.java
+++ src/main/java/com/epf/rentmanager/service/ClientService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.epf.rentmanager.dao.ClientDao;
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.model.Client;
 
 public class ClientService {
 
